47165af9138a9c8e020f7db159c25ac7c0037e3d
Victor Vasilev <victor.vasilev98@gmail.com>
2023-12-10 01:01:29 +0200

    Format Source.cpp correctly

--- DX12Project/Source.cpp
+++ DX12Project/Source.cpp
@@ -5,39 +5,40 @@
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int main() {
-    HINSTANCE hInstance = GetModuleHandle(NULL);
 
-    WNDCLASS wc = {};
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = L"DX12Project";
-    RegisterClass(&wc);
+	HINSTANCE hInstance = GetModuleHandle(NULL);
 
-    HWND hwnd = CreateWindow(
-        L"DX12Project", L"DX12Project",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
-        0, 0, hInstance, 0
-    );
+	WNDCLASS wc = {};
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = L"DX12Project";
+	RegisterClass(&wc);
 
-    ShowWindow(hwnd, SW_SHOWNORMAL);
-    UpdateWindow(hwnd);
+	HWND hwnd = CreateWindow(
+		L"DX12Project", L"DX12Project",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+		0, 0, hInstance, 0
+	);
 
-    MSG msg = {};
-    while (GetMessage(&msg, 0, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
+	ShowWindow(hwnd, SW_SHOWNORMAL);
+	UpdateWindow(hwnd);
+
+	MSG msg = {};
+	while (GetMessage(&msg, 0, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
 	return 0;
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            return 0;
-        default:
-            return DefWindowProc(hwnd, uMsg, wParam, lParam);
-    }
+	switch (uMsg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
 }
