5da3d216117e82794b45c88927c0d5d10e465348
Victor Vasilev <victor.vasilev98@gmail.com>
2023-12-10 00:47:38 +0200

    Create Windows window

--- DX12Project/Source.cpp
+++ DX12Project/Source.cpp
@@ -1,7 +1,43 @@
-#include <iostream>
+#include <windows.h>
+#include <d3d12.h>
+#include <dxgi1_6.h>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int main() {
-	std::cout << "Hello World!" << std::endl;
+    HINSTANCE hInstance = GetModuleHandle(NULL);
+
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"DX12Project";
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindow(
+        L"DX12Project", L"DX12Project",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+        0, 0, hInstance, 0
+    );
+
+    ShowWindow(hwnd, SW_SHOWNORMAL);
+    UpdateWindow(hwnd);
+
+    MSG msg = {};
+    while (GetMessage(&msg, 0, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
 	return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
 }
